15f9cc5e18961472400b070cf5a762eeec51e968
Rerrah <rerrah57@gmail.com>
2026-05-25 21:53:56 +0900

    Modify app service list

--- app/apps/contents.ts
+++ app/apps/contents.ts
@@ -2,16 +2,6 @@ import type { App } from "./types";
 
 // Sample data
 export const apps: App[] = [
-  {
-    name: "BambooTracker",
-    description:
-      "A chiptune tracker for Yamaha YM2608 sound chip which was used in NEC PC-8801/9801 series computers.",
-    platform: ["windows", "mac", "linux"],
-    urlType: "web",
-    url: "https://bambootracker.github.io/BambooTracker/",
-    imageUrl: "/img/bambootracker.png?height=200&width=400",
-    isLcpImage: true,
-  },
     {
     name: "OPNShare",
     description:
@@ -21,15 +11,6 @@ export const apps: App[] = [
     url: "https://opnshare.vercel.app",
     imageUrl: "/img/opnshare.png?height=200&width=400",
     isLcpImage: true,
-  },
-  {
-    name: "YM2608 Tone Editor",
-    description:
-      "A tone editor for YM2608 sound chip, with support for converting tone data from various chiptune composition software.",
-    platform: ["windows"],
-    urlType: "download",
-    url: "https://github.com/rerrahkr/YM2608-Tone-Editor/releases",
-    imageUrl: "/img/ym2608-tone-editor.png?height=200&width=400",
   },
   {
     name: "ConvFMML",
